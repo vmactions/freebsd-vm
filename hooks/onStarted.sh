394d00a24b2cf69154caa62c1ac0fa21f4893262
neil <github@neilpang.com>
2022-07-29 09:45:47 +0800

    fix ntpdate

--- hooks/onStarted.sh
+++ hooks/onStarted.sh
@@ -1,9 +1,15 @@
 #run in the vm as soon as the vm is started, before run the "prepare"
 
 
+for i in $(seq 1 10); do 
+  echo "Try ntpdate... $1"
+  if ntpdate -b pool.ntp.org || (sleep 2; ntpdate -b us.pool.ntp.org )|| ( sleep 2; ntpdate -b asia.pool.ntp.org) || (sleep 2; ntpdate -b 0.ubuntu.pool.ntp.org); then
+    echo "ntpdate OK"
+    break;
+  fi
+done
 
 
-ntpdate -b pool.ntp.org || (sleep 2; ntpdate -b us.pool.ntp.org )|| ( sleep 2; ntpdate -b asia.pool.ntp.org) || (sleep 2; ntpdate -b 0.ubuntu.pool.ntp.org)
 
 
 
