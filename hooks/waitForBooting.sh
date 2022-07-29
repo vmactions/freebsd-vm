b85d3b6abf80e0b58cc0f2b6e4414dc0523a861a
neil <github@neilpang.com>
2022-07-29 12:59:27 +0800

    minor

--- hooks/waitForBooting.sh
+++ hooks/waitForBooting.sh
@@ -2,7 +2,7 @@
 
 
 echo "====> Wait for Boot Multi user"
-if bash vbox.sh waitForText $VM_OS_NAME "Boot Multi user" 5 ; then
+if bash vbox.sh waitForText $VM_OS_NAME "Boot Multi user" 8 ; then
   echo "====> OK, enter"
   bash vbox.sh enter
 fi
