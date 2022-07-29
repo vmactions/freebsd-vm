7bdf527f9527db5cb781f6d03db2d9a1c2b5474e
neil <github@neilpang.com>
2022-07-29 10:26:56 +0800

    enabled ntpd

--- hooks/onStarted.sh
+++ hooks/onStarted.sh
@@ -3,13 +3,10 @@
 #run in the vm as soon as the vm is started, before run the "prepare"
 
 
-for i in $(seq 1 10); do 
-  echo "Try ntpdate... $i"
-  if ntpdate -b pool.ntp.org || (sleep 2; ntpdate -b us.pool.ntp.org )|| ( sleep 2; ntpdate -b asia.pool.ntp.org) || (sleep 2; ntpdate -b 0.ubuntu.pool.ntp.org); then
-    echo "ntpdate OK"
-    break;
-  fi
-done
+service ntpd enable
+
+service ntpd start
+
 
 
 
