469785c553acd8953c5f79698f19a3a6dc8a1629
Neilpang <Neilpang@users.noreply.github.com>
2022-09-12 04:00:16 +0000

    Generated from https://github.com/vmactions/base-vm

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ async function setup(nat, mem) {
     await shell("pwd && ls -lah" );
     await shell("bash -c 'pwd && ls -lah ~/.ssh/ && cat ~/.ssh/config'" );
     core.setFailed(error.message);
+    throw error;
   }
 }
 
