656dbe809a93d37cd67dce08aff9996b1e602ef9
Brett Jia <dev.bjia56@gmail.com>
2024-12-11 21:26:43 -0500

    use correct quote

--- run.sh
+++ run.sh
@@ -283,7 +283,7 @@ setupNFSShare() {
   echo "Configuring NFS"
 
   ssh "$osname" sh <<EOF
-echo 'nfsuserd_enable="YES"" >> /etc/rc.conf
+echo 'nfsuserd_enable="YES"' >> /etc/rc.conf
 echo 'nfsuserd_flags="-domain srv.world"' >> /etc/rc.conf
 mount -t nfs -o nfsv4 192.168.122.1:$HOME/work $HOME/work/
 tree $HOME/work
