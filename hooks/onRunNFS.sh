71e50c7d35cc640f98c832fb2420798116102ded
Brett Jia <dev.bjia56@gmail.com>
2024-12-24 08:38:29 -0500

    use RUNNER_HOME

--- hooks/onRunNFS.sh
+++ hooks/onRunNFS.sh
@@ -1,3 +1,3 @@
 echo 'nfsuserd_enable="YES"' >> /etc/rc.conf
 echo 'nfsuserd_flags="-domain srv.world"' >> /etc/rc.conf
-mount -t nfs -o nfsv4 192.168.122.1:$HOME/work $HOME/work/
+mount -t nfs -o nfsv4 192.168.122.1:$RUNNER_HOME/work $RUNNER_HOME/work/
