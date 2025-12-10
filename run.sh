e9f737cd4511051c0687b2b2829d0c1205ac36d5
Generate.yml <ghactions@vmactions.org>
2025-12-10 02:56:02 +0000

    Generated from base-vm

--- run.sh
+++ run.sh
@@ -294,13 +294,14 @@ scpBackFromVM() {
     echo "==> Downloading files from $target_host:$remote_dir to $local_dir ..."
     #use gtar for solaris, because tar in solaris does not support --exclude
     #use find for openbsd
+    #use cpio for omnios
     ssh "$target_host" "cd \"$remote_dir\" && { 
         if command -v gtar >/dev/null 2>&1; then 
             gtar -cf - --exclude .git .
         elif tar --version >/dev/null 2>&1 && { tar --version 2>&1 | grep GNU >/dev/null || tar --version 2>&1 | grep bsdtar >/dev/null; }; then
             tar -cf - --exclude .git .
         else 
-            find . -name .git -prune -o -type f -print | tar -cf - -I -
+            find . -name .git -prune -o -type f -print | cpio -o -H ustar
         fi
     }" | tar -xf - -C "$local_dir"
     echo "==> Done."
