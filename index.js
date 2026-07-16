b43aad2ff8ae3b7c4292de4fa0eb6d9c76b16d22
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2026-07-16 11:52:44 +0300

    Add --cpu-type

--- index.js
+++ index.js
@@ -521,6 +521,7 @@ async function main() {
     const inputOsName = core.getInput("osname").toLowerCase();
     const mem = core.getInput("mem");
     const cpu = core.getInput("cpu");
+    const cpuType = core.getInput("cpu-type");
     const nat = core.getInput("nat");
     const envs = core.getInput("envs");
     const prepare = core.getInput("prepare");
@@ -726,6 +727,9 @@ async function main() {
 
     if (cpu) {
       args.push("--cpu", cpu);
+    }
+    if (cpuType) {
+      args.push("--cpu-type", cpuType);
     }
     if (mem) {
       args.push("--mem", mem);
